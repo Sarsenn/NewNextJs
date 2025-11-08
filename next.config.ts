b161fdba7298527c9d9a610dd0286dc0bfd58f4d
Sarsen <blazer.fei@gmail.com>
2025-11-08 13:09:42 +0900

    Htag-component

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    root: __dirname, // <-- явно говорим, где корень проекта
+  },
 };
 
 export default nextConfig;
